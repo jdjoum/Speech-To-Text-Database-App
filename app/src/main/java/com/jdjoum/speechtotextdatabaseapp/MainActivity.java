024460ef2e07ad4483af502491f5e8b7685d54c3
jdjoum <jdjoum@hotmail.com>
2021-03-30 19:45:18 -0400

    Speech-To-Text Implementation

--- app/src/main/java/com/jdjoum/speechtotextdatabaseapp/MainActivity.java
+++ app/src/main/java/com/jdjoum/speechtotextdatabaseapp/MainActivity.java
@@ -1,14 +1,69 @@
 package com.jdjoum.speechtotextdatabaseapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Member Variables
+    protected static final int RESULT_SPEECH = 1;
+    private ImageButton btnSpeak;
+    private TextView tvText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Mapping Member Variables to View Elements
+        tvText = findViewById(R.id.tvText);
+        btnSpeak = findViewById(R.id.btnSpeak);
+
+        // Speak Button OnClick
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "en-US");
+                try {
+                    startActivityForResult(intent, RESULT_SPEECH);
+                    tvText.setText("");
+                } catch (ActivityNotFoundException e) {
+                    Toast.makeText(getApplicationContext(), "Speech-To-Text is not support on this device.", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                }
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case RESULT_SPEECH:
+                if(resultCode == RESULT_OK && data != null) {
+                    ArrayList<String> text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    tvText.setText(text.get(0));
+                    /*
+                    for(int i = 0; i < text.size(); i++) {
+                        System.out.println(i + " " + text.get(i));
+                    }
+                    */
+                }
+                break;
+        }
     }
 }
